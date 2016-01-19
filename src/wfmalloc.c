edd09696b851f8cc4ad123691fac5d58a2b08dd5
ashish-17 <ashishj1707@gmail.com>
2016-01-19 00:26:40 -0500

    Removed the redundant wrapping struct from malloc data

--- src/wfmalloc.c
+++ src/wfmalloc.c
@@ -10,21 +10,17 @@
 #include "includes/page.h"
 #include <stdlib.h>
 
-typedef struct wfmalloc_data {
-	local_pool_t* l_pool;
-	shared_pool_t* s_pool;
-} wfmalloc_data_t;
+static local_pool_t* l_pool = NULL;
+static shared_pool_t* s_pool = NULL;
 
-static wfmalloc_data_t *wf_data = NULL;
 
 void wfinit(int max_count_threads) {
-	wf_data = (wfmalloc_data_t*)malloc(sizeof(wfmalloc_data_t));
-	wf_data->l_pool = create_local_pool(max_count_threads);
-	wf_data->s_pool = create_shared_pool(max_count_threads);
+	l_pool = create_local_pool(max_count_threads);
+	s_pool = create_shared_pool(max_count_threads);
 }
 
 void* wfmalloc(size_t bytes, int thread_id) {
-	return malloc_block_from_pool(wf_data->l_pool, wf_data->s_pool, thread_id, bytes);;
+	return malloc_block_from_pool(l_pool, s_pool, thread_id, bytes);;
 }
 
 void wffree(void* ptr) {
