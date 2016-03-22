ff05d3c49c34bb4806d247cbefecafb0b9448964
Archita Agarwal <archita@cs.brown.edu>
2016-03-22 10:58:17 -0400

    Changed a little in help_enq_finish to make it compatible with paper. DONOT remove TODOs from the file. They are hints for potential bugs in the future.

--- src/queue.c
+++ src/queue.c
@@ -255,6 +255,8 @@ bool is_pending(wf_queue_op_head_t* op_desc, long phase, int thread_id) {
 	return ((op_desc_ref->pending == 1) && (op_desc_ref->phase <= phase));
 }
 
+// TODO: note: stamp of node->next is not really used. It is always one. 
+// Can be a source of future bugs.
 void help_enq(wf_queue_head_t* queue, wf_queue_op_head_t* op_desc, int thread_id, int thread_to_help, long phase) {
 	LOG_PROLOG();
 
@@ -295,10 +297,10 @@ void help_finish_enq(wf_queue_head_t* queue, wf_queue_op_head_t* op_desc, int th
 	LOG_PROLOG();
 
 	wf_queue_node_t* tail_stamped_ref = queue->tail;
-	wf_queue_node_t* tail_ref = GET_PTR_FROM_TAGGEDPTR(queue->tail, wf_queue_node_t);
+	wf_queue_node_t* tail_ref = GET_PTR_FROM_TAGGEDPTR(tail_stamped_ref, wf_queue_node_t);
 
 	wf_queue_node_t *next_stamped_ref = tail_ref->next;
-	uint32_t old_stamp_tail = GET_TAG_FROM_TAGGEDPTR(queue->tail);
+	uint32_t old_stamp_tail = GET_TAG_FROM_TAGGEDPTR(tail_stamped_ref);
 	uint32_t new_stamp_tail = old_stamp_tail + 1;
 
 	if (GET_PTR_FROM_TAGGEDPTR(next_stamped_ref, wf_queue_node_t) != NULL) {
@@ -318,6 +320,7 @@ void help_finish_enq(wf_queue_head_t* queue, wf_queue_op_head_t* op_desc, int th
 			new_op_desc_ref->phase = old_op_desc_ref->phase;
 			new_op_desc_ref->pending = false;
 			new_op_desc_ref->enqueue = true;
+			// TODO: difference from paper: paper sets node to next
 			new_op_desc_ref->node = NULL;
 			new_op_desc_ref->queue = queue;
 
